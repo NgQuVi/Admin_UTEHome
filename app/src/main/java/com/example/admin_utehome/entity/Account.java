fe758b278e5c30d77b12c6e6e10144674e573888
huy2222 <nguyennhathuy21112005@gmail.com>
2026-03-25 11:50:49 +0700

    them entity

---
+++ app/src/main/java/com/example/admin_utehome/entity/Account.java
@@ -0,0 +1,27 @@
+package com.example.admin_utehome.entity;
+
+import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+@Entity(
+    tableName = "accounts",
+    indices = {@Index(value = "phone", unique = true)}
+)
+public class Account {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public String phone;
+    public String password;
+    public String role;
+    public boolean isActive = true;
+
+    public Account(String phone, String password, String role) {
+        this.phone = phone;
+        this.password = password;
+        this.role = role;
+        this.isActive = true;
+    }
+}
---
+++ app/src/main/java/com/example/admin_utehome/entity/Feedback.java
@@ -0,0 +1,15 @@
+package com.example.admin_utehome.entity;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "feedbacks")
+public class Feedback {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    public int accountId;
+    public String title;
+    public String content;
+    public String status;
+    public String createdAt;
+}
